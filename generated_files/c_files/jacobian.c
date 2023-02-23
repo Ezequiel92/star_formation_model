3ec5303b374503feef2e0677ef7c4f218cc13edd
Ezequiel <elozano@df.uba.ar>
2023-02-23 11:20:01 -0300

    Update dependencies

--- generated_files/c_files/jacobian.c
+++ generated_files/c_files/jacobian.c
@@ -26,22 +26,22 @@ static int jacobian(double t, const double y[], double *dfdy, double dfdt[], voi
 	gsl_matrix_set(m, 0, 0, -16.346836800000002 * y[0] * rho_C);
 	gsl_matrix_set(m, 0, 1, 0);
 	gsl_matrix_set(m, 0, 2, 0.0003885752566316025 * (eta_i + R) * aux_var);
-	gsl_matrix_set(m, 0, 3, -0.00019428762831580126 * (1.0 / aux_var) * (eta_i + R) * y[2] * rho_C);
+	gsl_matrix_set(m, 0, 3, (-0.0003885752566316025 * (eta_i + R) * y[2] * rho_C) / (2 * aux_var));
 
 	gsl_matrix_set(m, 1, 0, 16.346836800000002 * y[0] * rho_C);
-	gsl_matrix_set(m, 1, 1, 0.2826053731343284 * (1 - y[3]) * (-1.34e-5 - Z) * rho_C);
+	gsl_matrix_set(m, 1, 1, 0.2826053731343284 * (1.34e-5 + Z) * (-1 + y[3]) * rho_C);
 	gsl_matrix_set(m, 1, 2, 0.0003885752566316025 * (-1 * eta_i + eta_d) * aux_var);
-	gsl_matrix_set(m, 1, 3, -0.2826053731343284 * (-1.34e-5 - Z) * y[1] * rho_C - 0.00019428762831580126 * (-1 * eta_i + eta_d) * (1.0 / aux_var) * y[2] * rho_C);
+	gsl_matrix_set(m, 1, 3, (-0.0003885752566316025 * (-1 * eta_i + eta_d) * y[2] * rho_C) / (2 * aux_var) + 0.2826053731343284 * (1.34e-5 + Z) * y[1] * rho_C);
 
 	gsl_matrix_set(m, 2, 0, 0);
-	gsl_matrix_set(m, 2, 1, 0.2826053731343284 * (1.34e-5 + Z) * (1 - y[3]) * rho_C);
+	gsl_matrix_set(m, 2, 1, 0.2826053731343284 * (1 - y[3]) * (1.34e-5 + Z) * rho_C);
 	gsl_matrix_set(m, 2, 2, 0.0003885752566316025 * (-1 - eta_d) * aux_var);
-	gsl_matrix_set(m, 2, 3, -0.2826053731343284 * (1.34e-5 + Z) * y[1] * rho_C - 0.00019428762831580126 * (-1 - eta_d) * (1.0 / aux_var) * y[2] * rho_C);
+	gsl_matrix_set(m, 2, 3, (-0.0003885752566316025 * (-1 - eta_d) * y[2] * rho_C) / (2 * aux_var) - 0.2826053731343284 * (1.34e-5 + Z) * y[1] * rho_C);
 
 	gsl_matrix_set(m, 3, 0, 0);
 	gsl_matrix_set(m, 3, 1, 0);
 	gsl_matrix_set(m, 3, 2, 0.0003885752566316025 * (1 - R) * aux_var);
-	gsl_matrix_set(m, 3, 3, -0.00019428762831580126 * (1 - R) * (1.0 / aux_var) * y[2] * rho_C);
+	gsl_matrix_set(m, 3, 3, (-0.0003885752566316025 * (1 - R) * y[2] * rho_C) / (2 * aux_var));
 
 	dfdt[0] = 0;
 	dfdt[1] = 0;
