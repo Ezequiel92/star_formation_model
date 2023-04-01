dd945ec082bdc3741a00a3f895c58f5c60e28ae2
Ezequiel92 <lozano.ez@gmail.com>
2023-04-01 03:12:48 +0000

    Deploying to gh-pages from @ Ezequiel92/star_formation_model@5c077b2d7a89760636e34531ebe706bc3f12d181 🚀

--- generated_files/c_files/ez_sfr.c
+++ generated_files/c_files/ez_sfr.c
@@ -16,6 +16,7 @@
  *                static double interpolate1D(double x, const void *table)
  *                static double interpolate2D(double x, double y, const void *table)
  *                static int sf_ode(double t, const double y[], double f[], void *parameters)
+ *                static int jacobian(double t, const double y[], double *dfdy, double dfdt[], void *parameters)
  *                static void integrate_ode(const double *ic, double *parameters, double it, double *fractions)
  *                static double compute_sfr(const int index)
  *                double rate_of_star_formation(const int index)
@@ -74,9 +75,9 @@ void *read_ftable(const char *file_path, const int n_rows, const int n_cols)
   fclose(file_ptr);
 
   data_table *table = malloc(sizeof(data_table));
-  table->data   = data;
-  table->n_rows = n_rows;
-  table->n_cols = n_cols;
+  table->data       = data;
+  table->n_rows     = n_rows;
+  table->n_cols     = n_cols;
 
   return (void *)table;
 }
@@ -352,6 +353,83 @@ static int sf_ode(double t, const double y[], double f[], void *parameters)
   return GSL_SUCCESS;
 }
 
+/*! \brief Evaluate the Jacobian of the systems of equations.
+ *
+ *  Evaluate the Jacobian matrix of the model, using the following variables:
+ *
+ *  Ionized gas fraction:    if(t) = ρi(t) / ρC --> y[0]
+ *  Atomic gas fraction:     af(t) = ρa(t) / ρC --> y[1]
+ *  Molecular gas fraction:  mf(t) = ρm(t) / ρC --> y[2]
+ *  Stellar fraction:        sf(t) = ρs(t) / ρC --> y[3]
+ *
+ *  where ρC = ρi(t) + ρa(t) + ρm(t) + ρs(t) is the total density of the gas cell,
+ *  and each equation has units of Myr^(-1).
+ *
+ *  \param[in] t Unused variable to comply with the `gsl_odeiv2_driver_alloc_y_new()` API.
+ *  \param[in] y Values of the variables at which the Jacobian will be evaluated.
+ *  \param[out] dfdy Where the results of evaluating the Jacobian will be stored.
+ *  \param[out] dfdt Where the results of evaluating the time derivatives will be stored.
+ *  \param[in] parameters Parameters for the Jacobian.
+ *
+ *  \return Constant `GSL_SUCCESS`, to confirm that the computation was successful.
+ */
+static int jacobian(double t, const double y[], double *dfdy, double dfdt[], void *parameters)
+{
+  (void)(t);
+
+  /*
+   * Destructure the parameters
+   *
+   * rho_C: Total cell density [mp * cm⁻³]
+   * Z:     Metallicity [dimensionless]
+   * eta_d: Photodissociation efficiency of Hydrogen molecules [dimensionless]
+   * eta_i: Photoionization efficiency of Hydrogen atoms [dimensionless]
+   * R:     Mass recycling fraction [dimensionless]
+   */
+  double *p    = (double *)parameters;
+  double rho_C = p[0];
+  double Z     = p[1];
+  double eta_d = p[2];
+  double eta_i = p[3];
+  double R     = p[4];
+
+  gsl_matrix_view dfdy_mat = gsl_matrix_view_array(dfdy, 4, 4);
+  gsl_matrix *m            = &dfdy_mat.matrix;
+
+  double aux_var = sqrt((1.0 - y[3]) * rho_C);
+
+  gsl_matrix_set(m, 0, 0, -16.346836800000002 * y[0] * rho_C);
+  gsl_matrix_set(m, 0, 1, 0);
+  gsl_matrix_set(m, 0, 2, 0.0003885752566316025 * (eta_i + R) * aux_var);
+  gsl_matrix_set(m, 0, 3, (-0.0003885752566316025 * (eta_i + R) * y[2] * rho_C) / (2 * aux_var));
+
+  gsl_matrix_set(m, 1, 0, 16.346836800000002 * y[0] * rho_C);
+  gsl_matrix_set(m, 1, 1, 0.2826053731343284 * (1.34e-5 + Z) * (-1 + y[3]) * rho_C);
+  gsl_matrix_set(m, 1, 2, 0.0003885752566316025 * (-1 * eta_i + eta_d) * aux_var);
+  gsl_matrix_set(m, 1, 3,
+                 (-0.0003885752566316025 * (-1 * eta_i + eta_d) * y[2] * rho_C) / (2 * aux_var) +
+                     0.2826053731343284 * (1.34e-5 + Z) * y[1] * rho_C);
+
+  gsl_matrix_set(m, 2, 0, 0);
+  gsl_matrix_set(m, 2, 1, 0.2826053731343284 * (1 - y[3]) * (1.34e-5 + Z) * rho_C);
+  gsl_matrix_set(m, 2, 2, 0.0003885752566316025 * (-1 - eta_d) * aux_var);
+  gsl_matrix_set(
+      m, 2, 3,
+      (-0.0003885752566316025 * (-1 - eta_d) * y[2] * rho_C) / (2 * aux_var) - 0.2826053731343284 * (1.34e-5 + Z) * y[1] * rho_C);
+
+  gsl_matrix_set(m, 3, 0, 0);
+  gsl_matrix_set(m, 3, 1, 0);
+  gsl_matrix_set(m, 3, 2, 0.0003885752566316025 * (1 - R) * aux_var);
+  gsl_matrix_set(m, 3, 3, (-0.0003885752566316025 * (1 - R) * y[2] * rho_C) / (2 * aux_var));
+
+  dfdt[0] = 0;
+  dfdt[1] = 0;
+  dfdt[2] = 0;
+  dfdt[3] = 0;
+
+  return GSL_SUCCESS;
+}
+
 /*! \brief Solve the system of ODEs, using numerical integration.
  *
  * ICs:
@@ -383,47 +461,17 @@ static void integrate_ode(const double *ic, double *parameters, double it, doubl
 #endif /* #ifdef TESTING */
 {
   /* Initial conditions */
-  double i0 = ic[0];
-  double a0 = ic[1];
-  double m0 = ic[2];
-  double s0 = ic[3];
+  fractions[0] = ic[0];
+  fractions[1] = ic[1];
+  fractions[2] = ic[2];
+  fractions[3] = ic[3];
+  double t0    = 0.0;
 
-  /* Initialize integration variables */
-  double t0  = 0.0;
-  double i_f = 0.0;
-  double a_f = 0.0;
-  double m_f = 0.0;
-  double s_f = 0.0;
+  gsl_odeiv2_system sys     = {sf_ode, jacobian, N_EQU, parameters};
+  gsl_odeiv2_driver *driver = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk4imp, it * 1e-3, 1e-6, 0.0);
 
-  /* Save mean cell density */
-  double rhoC = parameters[0];
-
-  gsl_odeiv2_system sys     = {sf_ode, NULL, N_EQU, parameters};
-  gsl_odeiv2_driver *driver = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_msadams, it * 1e-3, 1e-6, 0.0);
-
-  /* Use a ISM density PDF model to increase the effective resolution of the computation */
-  for(size_t i = 0; i < DIVISIONS; ++i)
-    {
-      double y[]    = {i0, a0, m0, s0};
-      parameters[0] = rhoC * F_RHO[i];
-
-      gsl_odeiv2_driver_reset(driver);
-      gsl_odeiv2_driver_apply(driver, &t0, it, y);
-
-      i_f += y[0] * PDF[i];
-      a_f += y[1] * PDF[i];
-      m_f += y[2] * PDF[i];
-      s_f += y[3] * PDF[i];
-
-      t0 = 0.0;
-    }
-
+  gsl_odeiv2_driver_apply(driver, &t0, it, fractions);
   gsl_odeiv2_driver_free(driver);
-
-  fractions[0] = i_f;
-  fractions[1] = a_f;
-  fractions[2] = m_f;
-  fractions[3] = s_f;
 }
 
 #ifndef TESTING
